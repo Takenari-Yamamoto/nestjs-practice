3015fad3e0027cde6b28486b21d33bd7c9e0acc4
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-06-02 19:35:05 +0900

    いったんコミット

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
 import { ItemsService } from './items.service';
@@ -7,8 +15,13 @@ import { ItemsService } from './items.service';
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
   }
 
   @Post()
@@ -27,5 +40,15 @@ export class ItemsController {
       status: ItemStatus.ON_SALE,
     };
     return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
   private items: Item[] = [];
+
   findAll() {
-    return 'This is items service';
+    return this.items;
   }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
   create(item: Item): Item {
     console.log(item);
     this.items.push(item);
     return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
+    return item;
+  }
+
+  delete(id: string): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
